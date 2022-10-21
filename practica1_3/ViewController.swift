7e69127fb228e92d60572a357f7a4e4f37a027ed
Fer <reikafer@gmail.com>
2022-10-21 07:34:00 -0700

    Initial Commit

---
+++ practica1_3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practica1_3
+//
+//  Created by Alumno on 10/21/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
