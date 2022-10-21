26105bb16b223d37ae472643025b586d44940041
Fer <reikafer@gmail.com>
2022-10-21 08:04:14 -0700

    practica

--- practica1_3/ViewController.swift
+++ practica1_3/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblAdvertencia: UILabel!
+    @IBOutlet weak var swPermisoNavegacion: UISwitch!
+    @IBOutlet weak var lblTexto: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblAdvertencia.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func doTapModificar(_ sender: Any) {
+        lblAdvertencia.isHidden = true
+        if swPermisoNavegacion.isOn {
+            performSegue(withIdentifier: "goToModificar", sender: self)
+        } else {
+           lblAdvertencia.isHidden = false
+        }
+    }
+    
 }
 
